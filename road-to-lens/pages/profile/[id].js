a92c67780e92225d0f17535f6796ac3978fbe6f4
MSBivens <mikebiv95@gmail.com>
2022-07-14 13:19:31 -0500

    updated posts

--- road-to-lens/pages/profile/[id].js
+++ road-to-lens/pages/profile/[id].js
@@ -1,9 +1,8 @@
-// pages/profile/[id].js
-
 import { useQuery, useMutation } from "@apollo/client";
 import { useRouter } from "next/router";
 import fetchProfileQuery from "../../queries/fetchProfileQuery.js";
 import Profile from "../../components/Profile.js";
+import Post from "../../components/Post.js";
 
 export default function ProfilePage() {
   const router = useRouter();
@@ -15,7 +14,7 @@ export default function ProfilePage() {
       request: { profileId: id },
       publicationsRequest: {
         profileId: id,
-        publicationTypes: ["POST"], // We really only want POSTs
+        publicationTypes: ["POST"],
       },
     },
   });
@@ -23,10 +22,12 @@ export default function ProfilePage() {
   if (loading) return "Loading..";
   if (error) return `Error! ${error.message}`;
 
-  console.log("on profile page data: ", data);
   return (
     <div className="flex flex-col p-8 items-center">
       <Profile profile={data.profile} displayFullProfile={true} />
+      {data.publications.items.map((post, idx) => {
+        return <Post key={idx} post={post} />;
+      })}
     </div>
   );
 }
